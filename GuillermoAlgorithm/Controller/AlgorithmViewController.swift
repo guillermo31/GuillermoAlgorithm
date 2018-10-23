5a6209ba1b90d55752cede9600c3351110ede09d
Guillermo <memo33101@gmail.com>
2018-10-23 08:54:09 -0600

    Created a function with variables and a list that contains the algorithm's steps

--- GuillermoAlgorithm/Controller/AlgorithmViewController.swift
+++ GuillermoAlgorithm/Controller/AlgorithmViewController.swift
@@ -29,7 +29,28 @@ public class AlgorithmViewController: UIViewController
     
     private func formatAlgorithm() -> Void
     {
+        let stepOne : String = "1. Open Xcode and select the type of app you want to make(iOS, macOS, tvOS)"
+        let stepTwo : String = "2. Organize files into folders using MVC structure."
+        let stepThree : String = "3. Inside the controller file write the needed code for the app."
+        let stepFour :  String = "6. Inside the view file design the UI of the app."
+        let stepFive : String = "5. Utilize the built-in simulator or use an iOS device to test app."
+        let stepSix : String = "6. Repeat 3, 4, and 5 as needed."
+        
+        let algorithm = [stepOne, stepTwo, stepThree, stepFour, stepFive, stepSix]
+    }
+    
+    private func createOutlineStyle() -> NSParagraphStyle
+    {
+        let outlineStyle : NSMutableParagraphStyle = NSMutableParagraphStyle()
+        outlineStyle.alignment = .left
+        outlineStyle.defaultTabInterval = 15
+        outlineStyle.firstLineHeadIndent = 20
+        outlineStyle.headIndent = 35
+        
+        return outlineStyle
         
     }
+    
+    
 }
 

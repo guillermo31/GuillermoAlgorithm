065d0b84cc6e185cc08959646bae748ca4c946ef
Guillermo <memo33101@gmail.com>
2018-10-23 07:55:27 -0600

    Created project and organized it into MVC structure

---
+++ GuillermoAlgorithm/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuillermoAlgorithm
+//
+//  Created by Ramos aguirre, Guillermo on 10/23/18.
+//  Copyright © 2018 ctec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
